8f711cd5983672241958744bc214c89d8d6322e2
qq7866196 <qq7866196@users.noreply.github.com>
2017-09-01 17:49:14 +0800

    Add files via upload

---
+++ rxjavabus/src/main/java/com/fastcat/rxjavabus/Subscription.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2012-2016 Markus Junginger, greenrobot (http://greenrobot.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fastcat.rxjavabus;
+
+final class Subscription {
+    final ISubscriber subscriber;
+    volatile boolean active;
+
+    Subscription(ISubscriber subscriber) {
+        this.subscriber = subscriber;
+        active = true;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Subscription) {
+            Subscription otherSubscription = (Subscription) other;
+            //同一个类，默认只需要注册一次，再次注册需要先移除上次注册。
+            return  subscriber.getClass() == otherSubscription.subscriber.getClass();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return subscriber.hashCode();
+    }
+}
